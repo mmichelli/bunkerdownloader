a6c73c5c00289028d6c093ffe4690a476920ba11
Mario <m.michelli@krohne.com>
2020-11-02 14:18:02 +0100

    catch errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,8 @@ import datetime
 import base64
 from io import BytesIO
 
-st.sidebar.title("Bunker Downloader")
-st.sidebar.markdown("### Connect:")
-url = st.sidebar.text_input("URL", value="10.100.42.216")
-username = st.sidebar.text_input("Username")
-password = st.sidebar.text_input("Password", type='password')
-ecomate = MongoClient(f"mongodb://{username}:{password}@{url}/ecomate?authSource=admin")
-db = ecomate.get_database()
+
+
 
 @st.cache
 def get_companies():
@@ -57,45 +52,56 @@ def get_table_download_link(df):
     b64 = base64.b64encode(val)  # val looks like b'...'
     return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="extract.xlsx">Download csv file</a>'
 
+st.sidebar.title("Bunker Downloader")
+st.sidebar.markdown("### Connect:")
+url = st.sidebar.text_input("URL", value="10.100.42.216")
+username = st.sidebar.text_input("Username")
+password = st.sidebar.text_input("Password", type='password')
+
+if url and username and password:
+    ecomate = MongoClient(f"mongodb://{username}:{password}@{url}/ecomate?authSource=admin")
+    db = ecomate.get_database()
+else:
+    db = None
 if(db):
     st.sidebar.markdown("### Choose:")
 
-companies = get_companies()
-company = st.sidebar.selectbox("Choose a company", companies, format_func=lambda v: v["name"])
-vessels_list = get_vessels(company["pkId"])
-vessel = st.sidebar.selectbox("Choose a Vessel",
+    companies = get_companies()
+    company = st.sidebar.selectbox("Choose a company", companies, format_func=lambda v: v["name"])
+    vessels_list = get_vessels(company["pkId"])
+    vessel = st.sidebar.selectbox("Choose a Vessel",
                               vessels_list, format_func=lambda v: v["name"])
-bunkers = get_bunkerItems()
-bunker = st.sidebar.selectbox("Choose a bunker", bunkers, format_func=lambda v: v["startTime"])
-bunker_measurements = get_bunkerValues(bunker["pkId"])
-out = {m["type"]: m["values"] for m in bunker_measurements}
-index = bunker['startTime'] + np.arange(len(out["MassFlow"])) * datetime.timedelta(seconds=1)
-df = pd.DataFrame(out, index=index)
+    bunkers = get_bunkerItems()
+    bunker = st.sidebar.selectbox("Choose a bunker", bunkers, format_func=lambda v: v["startTime"])
+    bunker_measurements = get_bunkerValues(bunker["pkId"])
+    out = {m["type"]: m["values"] for m in bunker_measurements}
+    index = bunker['startTime'] + np.arange(len(out["MassFlow"])) * datetime.timedelta(seconds=1)
+    df = pd.DataFrame(out, index=index)
 
-st.markdown(f"### {vessel['name']}")
-st.markdown(f"**{bunker['startTime'].strftime('%d %b %Y, %H:%M')}** - **{bunker['endTime'].strftime('%d %b %Y, %H:%M')}**")
+    st.markdown(f"### {vessel['name']}")
+    st.markdown(f"**{bunker['startTime'].strftime('%d %b %Y, %H:%M')}** - **{bunker['endTime'].strftime('%d %b %Y, %H:%M')}**")
 
-st.markdown(get_table_download_link(df), unsafe_allow_html=True)
+    st.markdown(get_table_download_link(df), unsafe_allow_html=True)
 
-filtered = [{"Key": k, "Value": bunker[k]} for k in bunker.keys() if "_" not in k]
-bunker_pd = pd.DataFrame(filtered)
+    filtered = [{"Key": k, "Value": bunker[k]} for k in bunker.keys() if "_" not in k]
+    bunker_pd = pd.DataFrame(filtered)
 
-st.markdown("### Bunker Data")
-st.line_chart(df)
+    st.markdown("### Bunker Data")
+    st.line_chart(df)
 
-st.markdown("### Bunker details")
-bunker_pd
+    st.markdown("### Bunker details")
+    bunker_pd
 
-st.markdown("### Fuel details")
-fuels = get_FuelType(bunker["vesselId"], bunker["fuelTypeId"])
-for f in fuels:
-    st.markdown(f"Name: ** {f['name']}**")
-    st.markdown(f"Category: ** {f['category']} **")
-    st.markdown(f"CO2 Emission Factor: ** {f['CO2EmissionFactor']} **")
-    st.markdown(f"sulphur Content ** {f['sulphurContent']} **")
+    st.markdown("### Fuel details")
+    fuels = get_FuelType(bunker["vesselId"], bunker["fuelTypeId"])
+    for f in fuels:
+        st.markdown(f"Name: ** {f['name']}**")
+        st.markdown(f"Category: ** {f['category']} **")
+        st.markdown(f"CO2 Emission Factor: ** {f['CO2EmissionFactor']} **")
+        st.markdown(f"sulphur Content ** {f['sulphurContent']} **")
 
 
-st.markdown("---")
-for bm in bunker_measurements:
-    st.markdown(f"### {bm['type']}")
-    st.line_chart(df[bm["type"]])
+    st.markdown("---")
+    for bm in bunker_measurements:
+        st.markdown(f"### {bm['type']}")
+        st.line_chart(df[bm["type"]])
